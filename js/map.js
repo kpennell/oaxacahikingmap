6c0321e934c58214072de8cdc9cf2c14c6890218
Kyle Pennell <kpennell@gmail.com>
2014-03-19 16:44:01 +0000

    slight text tweaks and analytics

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ $('a.toggle').click(function () {
     return false;
 });
 
-var tiles = L.tileLayer("http://b.tile.cloudmade.com/427d7a7244db4c62b51b1ee252996cc0/1714/256/{z}/{x}/{y}.png", {
+var tiles = L.tileLayer("http://b.tile.cloudmade.com/427d7a7244db4c62b51b1ee252996cc0/124173/256/{z}/{x}/{y}.png", {
     maxZoom: 17,
     subdomains: ["otile1", "otile2", "otile3", "otile4"],
     attribution: 'Tiles by CloudMade.'
